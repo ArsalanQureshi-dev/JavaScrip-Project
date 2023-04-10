26dd1215ae6c467a3eab6d332c5e07e0daa466dc
Arsalan Qureshi <alikhan22125@gmail.com>
2023-04-10 21:57:20 +0500

    Task Complete

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,1 +1,6 @@
 // date 9-Apr
+var zakatPercentage = 0.025;
+var userInput = +prompt("Please Enter Amount:");
+  var result = zakatPercentage * userInput;
+  alert("Your Zakat Amount is " + result);
+
